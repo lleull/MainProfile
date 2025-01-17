114d9294ec70dfc86ea7a7fd30d893b7c7dd917d
Leul Mekonnen <leulmekonnen049@gmail.com>
2025-01-16 22:19:54 -0800

    font added

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,18 +1,24 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss'
 
 export default {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        background: 'var(--background)',
+        foreground: 'var(--foreground)',
       },
+    },
+
+    fontFamily: {
+      Jersey_10: ['jersey'],
+      titi: ['titi'],
+      titibold: ['titibold'], // Add your custom font
     },
   },
   plugins: [],
-} satisfies Config;
+} satisfies Config

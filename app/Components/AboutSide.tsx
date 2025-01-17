effda6fbbd6b92638d442f337b7df1306be5effb
Leul Mekonnen <leulmekonnen049@gmail.com>
2025-01-16 23:11:53 -0800

    About page added

---
+++ app/Components/AboutSide.tsx
@@ -0,0 +1,31 @@
+import React from 'react'
+
+export const AboutSide = () => {
+  return (
+    <div className="flex flex-col w-1/2   h-full justify-start items-start">
+    <h3 className="text-gray-400 pt-20 ">
+      Certainly! Here's a paragraph summarizing your software development journey: "My
+      software development journey began with a foundational understanding of{' '}
+      <strong className="text-gray-300">HTML and CSS,</strong>
+      enabling me to create basic websites. I then delved into{' '}
+      <strong className="text-gray-300">JavaScript</strong> , progressing steadily and
+      enhancing my learning through the utilization of frameworks.{' '}
+      <strong className="text-gray-300">React and Next.js</strong>,
+    </h3>
+    <h3 className="text-gray-400 pt-5 ">
+      recognized as essential technologies in today's tech landscape, became my primary focus.
+      By building intricate and dynamic websites, I gained a deep understanding of these
+      frameworks. For styling,
+      <strong className="text-gray-300">I embraced Tailwind CSS</strong> , appreciating its
+      remarkable shortcuts and the seamless accessibility it provides across all my projects."
+    </h3>
+    <h3 className="text-gray-400 pt-5 ">
+      "I am a strong communicator with excellent written and verbal skills. I thrive in
+      collaborative environments and actively participate in team discussions, offering
+      constructive feedback and readily adapting to team dynamics. I possess strong
+      problem-solving and critical thinking abilities, enabling me to effectively address
+      challenges and find innovative solutions."
+    </h3>
+  </div>
+  )
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,22 +1,63 @@
-"use client"
-import { useState } from "react"
+'use client'
+import { useState } from 'react'
+import { AboutSide } from './Components/AboutSide'
 export default function Home() {
-  const [selectedOptions, setselectedOptions] = useState("about")
+  const [selectedOptions, setselectedOptions] = useState('About')
   return (
     <div className="w-full h-[100vh] bg-transparent flex items-center justify-center">
-      <div className="w-[80%] h-[80%] flex flex-row">
+      <div className="w-[80%] h-[80%] flex flex-row items-center justify-between">
         <div className="flex flex-col items-start ">
           <div className="flex flex-col items-start gap-5">
             <h1 className="text-[60px] font-Jersey font-semibold  text-white">Leul Mekonnen</h1>
             <h2 className="text-xl font-light  text-white">Exprienced Front-End Engineer</h2>
-            <p className="text-sm font-light  text-gray-500">I build exceptional and accessible digital experiences for the web</p>
+            <p className="text-sm font-light  text-gray-500">
+              I build exceptional and accessible digital experiences for the web
+            </p>
           </div>
-          <div className="flex flex-col items-start gap-5 mt-40">
-            <h1 className=" flex flex-row items-center gap-3 text-white"> <div className="w-24 h-[1px] bg-gray-400"/>About</h1>
-            <h1 className=" flex flex-row items-center gap-3 text-white"><div className="w-24 h-[1px] bg-gray-400"/>Expriences</h1>
-            <h1 className=" flex flex-row items-center gap-3 text-white"><div className="w-24 h-[1px] bg-gray-400"/>Projects</h1>
+          <div className="flex flex-col items-start gap-6 mt-28 ">
+            <h1
+              onClick={() => setselectedOptions('About')}
+              className={`flex flex-row items-center gap-3 hover:cursor-pointer ${
+                selectedOptions === 'About' ? 'text-white' : 'text-gray-500'
+              } `}
+            >
+              {' '}
+              <div
+                className={`${
+                  selectedOptions === 'About' ? 'w-32 bg-white h-[1px] ' : 'w-16 '
+                }h-[1px] bg-gray-600`}
+              />
+              About
+            </h1>
+            <h1
+              onClick={() => setselectedOptions('Expriences')}
+              className={`flex flex-row items-center gap-3 hover:cursor-pointer ${
+                selectedOptions === 'Expriences' ? 'text-white' : 'text-gray-500'
+              } `}
+            >
+              <div
+                className={` ${
+                  selectedOptions === 'Expriences' ? 'w-32 h-[1px] bg-white' : 'w-16 '
+                }h-[1px] bg-gray-600`}
+              />
+              Expriences
+            </h1>
+            <h1
+              onClick={() => setselectedOptions('Projects')}
+              className={`flex flex-row items-center gap-3 hover:cursor-pointer ${
+                selectedOptions === 'Projects' ? 'text-white' : 'text-gray-500'
+              } `}
+            >
+              <div
+                className={` ${
+                  selectedOptions === 'Projects' ? 'w-32 h-[1px] bg-white' : 'w-16 '
+                }h-[1px] bg-gray-600`}
+              />
+              Projects
+            </h1>
           </div>
         </div>
+        <AboutSide />
       </div>
     </div>
   )

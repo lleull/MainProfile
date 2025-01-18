49728b30665bf676f7b2f38ab740eb58229ed4bb
Leul Mekonnen <leulmekonnen049@gmail.com>
2025-01-17 21:27:23 -0800

    sunday commit

--- app/utils/Projects.tsx
+++ app/utils/Projects.tsx
@@ -9,7 +9,14 @@ const ProjectsData = [
   },
   {
     id: 2,
-    image: trip,
+    image: mtn,
+    role: 'Front-end Nextjs Developer',
+    desc: 'Project description. a company called MTN hired me as a software frontend engineer to build a landing website for there hiking trip and vacations company.',
+    skills: ['Typescript', 'Next.js', 'TailwindCss', 'ChatGpt', 'ReactJs'],
+  },
+  {
+    id: 3,
+    image: mtn,
     role: 'Front-end Nextjs Developer',
     desc: 'Project description. a company called MTN hired me as a software frontend engineer to build a landing website for there hiking trip and vacations company.',
     skills: ['Typescript', 'Next.js', 'TailwindCss', 'ChatGpt', 'ReactJs'],

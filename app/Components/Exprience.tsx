b352bb776bcef56eb3992dd75850dbe2b6c3411a
Leul Mekonnen <leulmekonnen049@gmail.com>
2025-01-19 04:33:39 -0800

    Exprience page finished

--- app/Components/Exprience.tsx
+++ app/Components/Exprience.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
-
+import { imageIcons } from '../utils/Projects'
+import Image from 'next/image'
 const Exprience = () => {
   return (
-    <div className='h-full w-full items-center flex justify-center'>
-      
-
+    <div className="h-full w-full items-center flex justify-center">
+      {imageIcons.map((img, i) => {
+        return <Image src={img.img1} alt="sd" key={i} className='bounceimg' />
+      })}
     </div>
   )
 }

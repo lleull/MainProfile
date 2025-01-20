2dca4cd7bfc495294f6ae8beebe04e429b074d48
Leul Mekonnen <leulmekonnen049@gmail.com>
2025-01-20 04:14:44 -0800

    Project slider added

--- app/Components/ProjectsSide.tsx
+++ app/Components/ProjectsSide.tsx
@@ -1,30 +1,36 @@
-import React from 'react'
+import React, { useState } from 'react'
 import arrow from './../assets/arrow.png'
 import Image from 'next/image'
 import ProjectsData from './ProjectData'
 const ProjectSide = () => {
+  const [countArray, setcountArray] = useState(1)
+
+  const handleNextProject = () => {
+    if (countArray === ProjectsData.length) {
+      setcountArray(1)
+    } else {
+      setcountArray(countArray + 1)
+    }
+  }
   return (
     <div className="flex flex-col w-1/2 mb-20  h-full justify-start items-start sm:w-[95%] sm:pl-10 ">
-      {/* {
-        ProjectsData.map((items, i) => {
-          return (
-
-          )
-        } )
-      } */}
-      <div>
-        <h3 className="text-gray-400 lg:pt-10  sm:pt-3">
-          <div className="flex flex-row items-center gap-2">
-            <h1 className="text-3xl text-white font-titibold">1</h1>
-            <h1 className="text-xl text-white">Borsa</h1>
+      {ProjectsData.map((data, i) => {
+        return data.id == countArray ? (
+          <div className='bg-red-300' key={i}>
+            <h3 className="text-gray-400 lg:pt-10  sm:pt-3">
+              <div className="flex flex-row items-center gap-2">
+                <h1 className="text-3xl text-white font-titibold">{data.id}</h1>
+                <h1 className="text-xl text-white">{data.name}</h1>
+              </div>
+              {data.desc}
+            </h3>
           </div>
-          A Mobile application build using React-Native , Typescript and Expo for bundling the
-          Javascript. My major role on this project was creating a user exprience on the frontend
-          side of the project.
-        </h3>
-      </div>
+        ) : (
+          ''
+        )
+      })}
 
-      <Image src={arrow} alt="s" className="arrow" />
+      <Image onClick={() => handleNextProject()} src={arrow} alt="s" className="arrow" />
     </div>
   )
 }

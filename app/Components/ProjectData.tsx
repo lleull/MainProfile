d586d8c4e6cb779720c348e4168830576903126b
Leul Mekonnen <leulmekonnen049@gmail.com>
2025-01-20 04:05:36 -0800

    Projects Side Added

---
+++ app/Components/ProjectData.tsx
@@ -0,0 +1,19 @@
+const ProjectsData = [
+  {
+    id: 1,
+    name: 'Borsa',
+    desc: ' A Mobile application build using React-Native , Typescript and Expo for bundling the   Javascript. My major role on this project was creating a user exprience on the frontend  side of the project.',
+  },
+  {
+    id: 2,
+    name: 'CoinBase',
+    desc: ' A Mobile application build using React-Native , Typescript and Expo for bundling the   Javascript. My major role on this project was creating a user exprience on the frontend  side of the project.',
+  },
+  {
+    id: 3,
+    name: 'Mtn Hiking',
+    desc: ' A Mobile application build using React-Native , Typescript and Expo for bundling the   Javascript. My major role on this project was creating a user exprience on the frontend  side of the project.',
+  },
+]
+
+export default ProjectsData
--- app/Components/ProjectsSide.tsx
+++ app/Components/ProjectsSide.tsx
@@ -1,21 +1,30 @@
 import React from 'react'
-import arrow from "./../assets/arrow.png"
+import arrow from './../assets/arrow.png'
 import Image from 'next/image'
+import ProjectsData from './ProjectData'
 const ProjectSide = () => {
   return (
     <div className="flex flex-col w-1/2 mb-20  h-full justify-start items-start sm:w-[95%] sm:pl-10 ">
-      <h3 className="text-gray-400 lg:pt-10  sm:pt-3">
-        <div className="flex flex-row items-center gap-2">
-          <h1 className="text-3xl text-white font-titibold">1</h1>
-          <h1 className="text-xl text-white">Borsa</h1>
-        </div>
-        A Mobile application build using React-Native , Typescript and Expo for bundling the
-        Javascript. My major role on this project was creating a user exprience on the frontend side
-        of the project.
-      </h3>
+      {/* {
+        ProjectsData.map((items, i) => {
+          return (
 
-      <Image src={arrow} alt='s' className='arrow'/>
-   
+          )
+        } )
+      } */}
+      <div>
+        <h3 className="text-gray-400 lg:pt-10  sm:pt-3">
+          <div className="flex flex-row items-center gap-2">
+            <h1 className="text-3xl text-white font-titibold">1</h1>
+            <h1 className="text-xl text-white">Borsa</h1>
+          </div>
+          A Mobile application build using React-Native , Typescript and Expo for bundling the
+          Javascript. My major role on this project was creating a user exprience on the frontend
+          side of the project.
+        </h3>
+      </div>
+
+      <Image src={arrow} alt="s" className="arrow" />
     </div>
   )
 }
